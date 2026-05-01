4c3815eb5cb20b5170c8dc33240b9218b8be9538
hoanganh310tp3 <hoanganh310tp3@gmail.com>
2026-05-01 14:26:24 +0700

    first commit

---
+++ basic makefile/inc/sum.h
@@ -0,0 +1,6 @@
+#ifdef _SUM_H
+#define _SUM_H
+
+int sum (int a, int b);
+
+#endif
---
+++ basic makefile/src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "sum.h"
+
+void main()
+{
+    int a = 1, b = 2;
+    printf("a + b = %d\n", sum(a, b));
+    return 0;
+}
